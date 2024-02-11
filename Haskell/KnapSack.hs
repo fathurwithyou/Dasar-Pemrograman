c062cfaaf7f867d9da00d233ce703382139dc606
Ellipsis <rizkyfathur326@gmail.com>
2024-02-12 00:12:54 +0700

    Add KnapSack and KnapSack2

--- Haskell/KnapSack.hs
+++ Haskell/KnapSack.hs
@@ -1,15 +1,20 @@
+module KnapSack where
+
 takeNth :: Int -> [Int] -> Int
 takeNth n li
   | n == 0 = head li
-  | otherwise = takeNth (n-1) (tail li)
+  | otherwise = takeNth (n - 1) (tail li)
 
 maxi :: Int -> Int -> Int
-maxi n m 
+maxi n m
   | n > m = n
   | otherwise = m
 
 knapSack :: Int -> Int -> [Int] -> [Int] -> Int
 knapSack n w val wt
   | n == 0 || w == 0 = 0
-  | takeNth (n-1) wt > w = knapSack (n-1) w val wt
-  | otherwise = maxi (takeNth (n-1) val + knapSack (n-1) (w-takeNth(n-1) wt) val wt) (knapSack (n-1) w val wt)
+  | takeNth (n - 1) wt > w = knapSack (n - 1) w val wt
+  | otherwise = maxi ambilLanjut lanjut
+  where
+    ambilLanjut = takeNth (n - 1) val + knapSack (n - 1) (w - takeNth (n - 1) wt) val wt
+    lanjut = knapSack (n - 1) w val wt
---
+++ Haskell/KnapSack2.hs
@@ -0,0 +1,24 @@
+module KnapSack2 where
+
+takeNth :: Int -> [Int] -> Int
+takeNth n li
+  | n == 0 = head li
+  | otherwise = takeNth (n - 1) (tail li)
+
+maxi :: Int -> Int -> Int
+maxi n m
+  | n > m = n
+  | otherwise = m
+
+knapSack :: Int -> Int -> [Int] -> [Int] -> Int
+knapSack n w val wt
+  | n == -1 || w == 0 = 0
+  | takeNth n wt > w = knapSack (n - 1) w val wt
+  | otherwise = maxi ambilLanjut (maxi lanjut ambilTerus)
+  where 
+    ambilLanjut = takeNth n val + knapSack (n - 1) (w - takeNth n wt) val wt
+    lanjut = knapSack (n - 1) w val wt
+    ambilTerus = takeNth n val + knapSack n (w - takeNth n wt) val wt
+    
+knapSack2 :: Int -> Int -> [Int] -> [Int] -> Int
+knapSack2 n = knapSack (n-1)
